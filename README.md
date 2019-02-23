fbe29d0b9e6ebc137de9886182852c694352feb1
voodoo <nohashanon@gmail.com>
2019-02-23 12:32:07 +0300

    react routes added

--- README.md
+++
@@ -1,3 +0,0 @@
-# adulis-web
-adulis is an ecommerce web application developed using Node.js and React.js
-It aims to provide a usable functionality for its users
--- config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    google:{
-        clientID:'810689622035-a18ln58178r0p47q1u2rkr37fmvsvcev.apps.googleusercontent.com',
-        clientSecret:'tKgpuRtmLF1KRFGdBBp8un5u'
-    }
-}
